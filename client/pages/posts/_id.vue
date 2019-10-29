47cbb6fbc241b54b9b10f6084bc9abb12bac03e7
n2ptune <n2ptune@outlook.com>
2019-10-29 12:17:37 +0900

    test ssr

---
+++ client/pages/posts/_id.vue
@@ -0,0 +1,17 @@
+<template>
+  <a-row>
+    <a-col :span="12">
+      <p>{{ id }}</p>
+    </a-col>
+  </a-row>
+</template>
+
+<script>
+export default {
+  asyncData({ route }) {
+    return {
+      id: route.params.id
+    }
+  }
+}
+</script>
